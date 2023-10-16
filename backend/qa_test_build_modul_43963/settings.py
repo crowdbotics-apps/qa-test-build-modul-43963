040c01a34b31dcf5ff91ebc33b742a6458b306fc
Saad Bin Abid <saad.abid@crowdbotics.com>
2023-10-16 19:22:17 +0500

    Update settings

--- backend/qa_test_build_modul_43963/settings.py
+++ backend/qa_test_build_modul_43963/settings.py
@@ -211,6 +211,10 @@ REST_AUTH = {
 
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    )
 }
 
 # Custom user model
